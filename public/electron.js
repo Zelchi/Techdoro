d4691678ce278ae263362dcae62a96823c7d9b7d
Zelchi <tininhakawaii@gmail.com>
2025-02-15 03:04:41 -0300

    fix: add Notification import to electron.js for timer event notifications

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, Notification } from 'electron';
 import { fileURLToPath } from "url";
 import path from "path";
 
