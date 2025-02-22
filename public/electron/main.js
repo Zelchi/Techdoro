2cfe1e1478ba9c82afa87f65b247f3ead34a4f9d
Zelchi <tininhakawaii@gmail.com>
2025-02-22 03:52:54 -0300

    feat: update version to 1.1.1, add react-router-dom, and refactor main.js for multiple windows

--- public/electron/main.js
+++ public/electron/main.js
@@ -1,6 +1,6 @@
 import { app, BrowserWindow, ipcMain, Notification, Tray, Menu } from 'electron';
 import { fileURLToPath } from "url";
-import { isDev } from './utils.js'
+import { isDev } from './utils.js';
 import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,8 +8,11 @@ const __dirname = path.dirname(__filename);
 
 const icon = path.join(__dirname, '../iconTechDoro.png');
 
-const createWindow = () => {
-    const win = new BrowserWindow({
+let winClock;
+let winTasks;
+
+const createClockWindow = () => {
+    winClock = new BrowserWindow({
         icon,
         frame: false,
         webPreferences: {
@@ -24,103 +27,48 @@ const createWindow = () => {
         minHeight: 600,
     });
 
-    (isDev()) ? win.loadURL('http://localhost:5173/') : win.loadFile(path.join(__dirname, '../index.html'))
+    (isDev()) ? winClock.loadURL('http://localhost:5173/#/clock') : winClock.loadFile(path.join(__dirname, '../index.html'), { hash: 'clock' });
 
-    ipcMain.on('minimizar', () => {
-        win.minimize();
-    })
-
-    ipcMain.on('maximizar', () => {
-        win.isMaximized() ? win.unmaximize() : win.maximize();
-    })
-
-    ipcMain.on('fechar', () => {
-        win.hide();
-    });
-
-    const notifica = new Notification({
-        silent: true,
-        icon,
-        timeoutType: 'default',
-    })
-
-    ipcMain.on('notifiTimeLong', () => {
-        notifica.title = 'Tempo acabou!';
-        notifica.body = 'Vai dar uma esticada nas pernas!';
-        notifica.show();
-    });
-
-    ipcMain.on('notifiTimeShort', () => {
-        notifica.title = 'Intervalo acabou!';
-        notifica.body = 'Retome os estudos imediatamente!!!';
-        notifica.show();
-    });
-
-    notifica.on('click', () => {
-        if (win.isMinimized()) {
-            win.show(); win.restore();
-        }
-        else {
-            win.show(); win.focus();
-        }
-    })
-
-    const tray = new Tray(icon);
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Mostrar Janela',
-            icon: path.join(__dirname, '../abrir.png'),
-            role: 'window',
-            type: 'normal',
-            click: () => {
-                win.show();
-            },
-        },
-        {
-            label: 'Fechar Techdoro',
-            icon: path.join(__dirname, '../sair.png'),
-            role: 'close',
-            type: 'normal',
-            click: () => {
-                win.close();
-            },
-        },
-    ]);
-    tray.setToolTip('Techdoro')
-    tray.setContextMenu(contextMenu)
-    tray.on('click', () => {
-        if (win.isVisible()) {
-            win.hide();
-        } else {
-            win.show();
-        }
+    winClock.on('closed', () => {
+        winClock = null;
     });
 };
 
-if (!app.requestSingleInstanceLock()) {
-    app.quit();
-} else {
-    app.on('second-instance', () => {
-        const win = BrowserWindow.getAllWindows()[0];
-        if (win) {
-            if (win.isMinimized()) {
-                win.show(); win.restore();
-            } else {
-                win.show(); win.focus();
-            }
+const createTasksWindow = () => {
+    winTasks = new BrowserWindow({
+        icon,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: true,
+            sandbox: false,
+            preload: path.join(__dirname, 'preload.cjs')
+        },
+        width: 500,
+        height: 600,
+        minWidth: 500,
+        minHeight: 600,
+    });
+
+    (isDev()) ? winTasks.loadURL('http://localhost:5173/#/tasks') : winTasks.loadFile(path.join(__dirname, '../index.html'), { hash: 'tasks' });
+
+    winTasks.on('closed', () => {
+        winTasks = null;
+    });
+};
+
+app.whenReady().then(() => {
+    createClockWindow();
+    createTasksWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createClockWindow();
+            createTasksWindow();
         }
     });
+});
 
-    app.whenReady().then(() => {
-        app.setName(app.name)
-        app.setAppUserModelId(app.name)
-        createWindow();
-        app.on("activate", () => {
-            if (BrowserWindow.getAllWindows().length === 0) createWindow();
-        });
-    });
-
-    app.on("window-all-closed", () => {
-        if (process.platform !== 'darwin') app.quit();
-    });
-}
+app.on("window-all-closed", () => {
+    if (process.platform !== 'darwin') app.quit();
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import './main.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import './main.css';
+import App from './App.tsx';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Pomodoro } from './components/Pomodoro';
+import { Tarefas } from './components/Tarefas';
+import { BarraJanela } from './components/BarraJanela.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/clock" element={<><BarraJanela /><Pomodoro /></>} />
+        <Route path="/tasks" element={<><BarraJanela /><Tarefas /></>} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </Provider>
-)
+);
